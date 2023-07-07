a7b3b9a926b42873e7d81f44c251945939680702
Rey Vincent R. Acosta <acostareyvincent@gmail.com>
2023-07-07 10:21:05 +0800

    Initial commit

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import { useState } from 'react';
+
+
+export default function Dashboard({ auth }) {
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                {/*
+                    <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+                        <div>hide this text on small screen</div>
+                    </div>
+                */}
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
